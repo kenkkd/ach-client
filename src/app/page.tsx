678bcfd9f90cede6343e23510ca4f4f73eafaf36
amdlab-yabushita <kazuhiko.yabushita@amd-lab.com>
2023-02-28 21:51:49 +0900

    レス部分をコンポーネントに

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "src/app/page.module.css";
-import {
-  AppShell,
-  Button,
-  Container,
-  Group,
-  Header,
-  Navbar,
-  Title,
-} from "@mantine/core";
+import { AppShell, Container, Header, Title } from "@mantine/core";
+import Response from "../components/Response";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,8 +20,10 @@ export default function Home() {
         }
       >
         {/* Your application here */}
-        <Container size="xs" px="xs">
-          <Group></Group>
+        <Container size="md" px="xs" className={styles.container}>
+          <Response />
+          <Response />
+          <Response />
         </Container>
       </AppShell>
     </main>
